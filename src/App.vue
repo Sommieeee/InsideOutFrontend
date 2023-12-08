d85bfb73f1e5d0323c0c36840be45965d9943d09
Chisomaga Ndumele <mac@Chisomagas-MacBook-Pro.local>
2023-12-08 14:30:52 +0000

    First screen

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+  <Header></Header>
+   <!-- <RouterView /> -->
+  </div> 
+</template>
+<script setup>
+import Header from './components/Header.vue';
+// export default{
+//   components: {
+//     Header
+//   }
+// }
+</script>
+
+<style scoped>
+</style>
